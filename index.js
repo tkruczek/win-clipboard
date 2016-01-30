bfd744c65a86a0550d68b8b97d57131a5fad6629
tkruczek <t.kruczek.pl@gmail.com>
2016-01-30 22:47:56 +0100

    add comments

--- index.js
+++ index.js
@@ -1,7 +1,32 @@
+/**
+ * win-clipboard module
+ * */
 var clipboard = require('bindings')('clipboard');
 
+/**
+ * @module win-clipboard/getFormatsCount
+ * @returns {Number} Number of formats available in the clipboard
+ * */
+
 exports.getFormatsCount = clipboard.getFormatsCount;
+
+/**
+* @module win-clipboard/getFormats
+* @returns {Array} Array of format ids
+* */
 exports.getFormats = clipboard.getFormats;
+
+/**
+ * @module win-clipboard/getFormatName
+ * @param {number} Id of format
+ * @return {String} Name of the format
+ * */
 exports.getFormatName = clipboard.getFormatName;
+
+/**
+ * @module win-clipboard/getData
+ * @param {Number} Format id
+ * @returns {String} Content of clipboard for given format
+ * */
 exports.getData = clipboard.getData;
 
